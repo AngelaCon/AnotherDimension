c8c2eccb5cae34337a66c8d3d82d261c79284af9
Angela Cai <angelacai@Acais-Mac.T-mobile.com>
2020-10-25 13:21:21 -0400

    largest works

--- Tester.java
+++ Tester.java
@@ -1,7 +1,8 @@
 public class Tester {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4};
+        int[] arr = {1,2,2,2,8,0};
         System.out.println(ArrayOps.sum(arr));
+        System.out.println(ArrayOps.largest(arr));
     }
 
 
