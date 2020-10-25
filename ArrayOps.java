af84ed5f1b5e08ab6c486393e3dd55b35e9e0730
Angela Cai <angelacai@Acais-Mac.T-mobile.com>
2020-10-25 13:16:04 -0400

    sum() words

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,8 +1,13 @@
 public class ArrayOps {
     public static void main(String[] args) {
-
     }
 
-    public static void
+    public static int sum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
 
 }
