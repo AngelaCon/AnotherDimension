c9d59da4dfd3263d53da458ab348786026311e24
Angela Cai <angelacai@Acais-Mac.T-mobile.com>
2020-10-25 13:15:32 -0400

    tested 1

--- Tester.java
+++ Tester.java
@@ -1,8 +1,8 @@
 public class Tester {
     public static void main(String[] args) {
-
+        int[] arr = {1,2,3,4};
+        System.out.println(ArrayOps.sum(arr));
     }
 
-    public static void
 
 }
