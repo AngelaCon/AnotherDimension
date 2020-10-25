c98ab3e936e9904b931b6e41a5b5e303934342bc
Angela Cai <angelacai@Acais-Mac.T-mobile.com>
2020-10-25 16:04:44 -0400

    sumRows works

--- ArrayOps.java
+++ ArrayOps.java
@@ -18,4 +18,33 @@ public class ArrayOps {
         return max;
      }
 
+     public static int[] sumRows(int[][] matrix) {
+         int[] arr = new int[matrix.length];
+         for (int i = 0; i < matrix.length; i++){
+             arr[i] = sum(matrix[i]);
+         }
+         return arr;
+     }
+
+     // public static  int[] largestInRows(int[][] matrix) {
+     //     int[] arr = new int[matrix.length];
+     //     for (int i = 0; i < matrix.length; i++){
+     //         arr[i] = largest(matrix[i]);
+     //     }
+     //     return arr;
+     // }
+
+
+
+
+     public static String arrToString(int[] arr){
+        if (arr.length == 0) return "{}";
+        String output = "{";
+        for (int i = 0; i < arr.length-1; i++){
+          output = output +  arr[i] + ", ";
+        }
+        output = output + arr[arr.length-1] + "}";
+        return output;
+}
+
 }
--- Tester.java
+++ Tester.java
@@ -1,8 +1,13 @@
 public class Tester {
     public static void main(String[] args) {
         int[] arr = {1,2,2,2,8,0};
+        int[][] arr2 = {{10,2},{2,2},{8,100}};
+
+        int[][]  A = {{1,0, 12,-1},{7,-2,2,1},{-5,-2,2,-9}};
+        int[] B  = {1,3,5};
         System.out.println(ArrayOps.sum(arr));
         System.out.println(ArrayOps.largest(arr));
+        System.out.println(ArrayOps.arrToString(ArrayOps.largestInRows(A)));
     }
 
 
